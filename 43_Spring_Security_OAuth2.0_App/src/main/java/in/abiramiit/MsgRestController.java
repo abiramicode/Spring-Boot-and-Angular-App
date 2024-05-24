be2003400292862bc3893d7f5809d2c85aad6cbd
abiramicode <34230155+abiramicode@users.noreply.github.com>
2024-05-24 20:33:23 +0530

    Add files via upload

---
+++ 43_Spring_Security_OAuth2.0_App/src/main/java/in/abiramiit/MsgRestController.java
@@ -0,0 +1,13 @@
+package in.abiramiit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController   
+public class MsgRestController {
+	
+	@GetMapping("/")
+	public String welcomeMsg() {
+		return "Welcome Abirami";
+	}
+}
